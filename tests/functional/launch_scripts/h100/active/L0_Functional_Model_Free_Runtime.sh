ad96cb1e37eafb0e03636744c81af00439f737fc
Charlie Truong <chtruong@nvidia.com>
2026-07-09 09:37:18 -0500

    test: add model-free runtime functional coverage
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

---
+++ tests/functional/launch_scripts/h100/active/L0_Functional_Model_Free_Runtime.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# CI_TIMEOUT=20
+# GPU_COUNT=x1
+set -xeuo pipefail
+
+coverage run -a --data-file="${COVERAGE_FILE:-.coverage}" --source=nemo_voice_agent -m pytest \
+    -o log_cli=true \
+    -o log_cli_level=INFO \
+    --disable-warnings \
+    -vs tests/functional/runtime \
+    -m "functional and not gpu and not pleasefixme"
